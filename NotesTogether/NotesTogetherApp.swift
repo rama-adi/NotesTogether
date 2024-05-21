d345096311b63e22cbc62aba48c2bacd72864909
ramaadi <17980909+rama-adi@users.noreply.github.com>
2024-05-22 00:53:45 +0700

    Initial Commit

---
+++ NotesTogether/NotesTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesTogetherApp.swift
+//  NotesTogether
+//
+//  Created by Rama Adi Nugraha on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
